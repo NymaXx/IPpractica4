9db0158135808e98efa2afaaecdad138cfb28744
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-02-26 14:51:28 -0500

    se dieron permisos de internet en el manifest uses-permission

--- app/src/main/java/com/example/ippractica4/MainActivity.java
+++ app/src/main/java/com/example/ippractica4/MainActivity.java
@@ -3,9 +3,18 @@ package com.example.ippractica4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private TextView num1;
+    private TextView num2;
+    private TextView num3;
+    private TextView num4;
+    private Button pingButton;
+    private Button hostButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
